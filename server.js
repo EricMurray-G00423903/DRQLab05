5ee84ccac19d32f5994504327b1d142b0a5585d6
EricMurray-G00423903 <g00423903@atu.ie>
2024-10-25 10:27:17 +0000

    added index.html and nodemon

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));  //USE HTML JS CSS Files from PUBLIC dir
+
 //Get req on the main page
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying'); //send this return to the page
-});
-
-//Listener to log server to port
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
 });
 
 //Error handling
@@ -25,8 +22,48 @@ app.get('/hello/:name', (req, res) => {
     res.send(`Hello ${name}`);
 });
 
+//Added second route for second name
 app.get('/hello/:name/:second', (req, res) => {
     const name = req.params.name;
     const second = req.params.second;
     res.send(`Hello ${name} ${second}`);
+});
+
+const path = require('path');
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.json({ movies });
+    res.status(201).json({ movies });
+});
+
+//Listener to log server to port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });

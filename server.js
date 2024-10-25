c8ac9a45c82795d59a9f0014c582b5298816d45b
EricMurray-G00423903 <g00423903@atu.ie>
2024-10-25 10:14:20 +0000

    added express JS and starting functionality

---
+++ server.js
@@ -0,0 +1,26 @@
+// Express server settings
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Get req on the main page
+app.get('/', (req, res) => {
+    res.send('Welcome to Data Respresentation & Querying'); //send this return to the page
+});
+
+//Listener to log server to port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');  //500 error returns Something Went Wrong
+});
+
+//Added route for hello
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
